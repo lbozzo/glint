6a7f541e7e8adc6e2af245533b629f546c8970e5
Lucas Bozzo <lucas.bozzo@Lucass-Mac-mini.local>
2024-07-06 16:51:33 +0100

    Upgrade storybook

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,16 +1,22 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [getAbsolutePath("@storybook/addon-essentials"), getAbsolutePath("@storybook/addon-interactions")],
+
   framework: {
-    name: '@storybook/react-vite',
+    name: getAbsolutePath("@storybook/react-vite"),
     options: {
       builder: {
         viteConfigPath: 'vite.config.ts',
       },
     },
   },
+
+  docs: {
+    autodocs: true
+  }
 };
 
 export default config;
@@ -18,3 +24,7 @@ export default config;
 // To customize your Vite configuration you can use the viteFinal field.
 // Check https://storybook.js.org/docs/react/builders/vite#configuration
 // and https://nx.dev/recipes/storybook/custom-builder-configs
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
